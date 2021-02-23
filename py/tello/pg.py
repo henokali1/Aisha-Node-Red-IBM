9d9e586bd13a973978a187b529d530f638d7bc7b
henokali1 <henokali1@gmail.com>
2021-02-23 13:48:57 +0400

    Live feed page on local server UI redesign

--- py/tello/pg.py
+++ py/tello/pg.py
@@ -1,3 +1,10 @@
+from flask import Flask
+from os import system
+from flask import Flask,jsonify,render_template, send_from_directory,Response
+from random import randint
+
+from numpy import imag
+
 from pynput import keyboard
 from time import sleep
 from djitellopy import tello
@@ -159,129 +166,159 @@ def on_release(key):
         # Stop listener
         return False
 
-lr, fb, ud, yv = 0, 0, 0, 0
-# me = tello.Tello()
-video_capture = cv2.VideoCapture('tv2.mp4')
-
-# me.connect()
-# me.streamoff()
-# sleep(2)
-# me.streamon()
-
-speed = 50
-rec_names = {}
-
-preSetFacePath = 'imgs/'
-onlyfiles = [f for f in listdir(preSetFacePath) if isfile(join(preSetFacePath, f))]
-
-known_face_encodings = []
-known_face_names = []
-
-for i in onlyfiles:
-    lable = i.split('.')[0]
-    fn = preSetFacePath + i
-    known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(fn))[0])
-    known_face_names.append(lable)
-
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-
 # Download face data from IBM database
 download_faces()
 
 
-def frame_stream_thread(name):
+def gen_frames():  # generate frame by frame from camera
     global process_this_frame
     while True:
-        # frame = me.get_frame_read().frame
-        ret, frame = video_capture.read()
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-            if name != 'Unknown':
-                print(f'Face detected:\t{name}')
-                if not(name in rec_names):
-                    rec_names[name]=name
-                    print(f'Generating Report for {name}')
-                    fn = f'imgs/report-imgs/{name}-report.jpg'
-                    print(fn)
-                    
-                    generate_report(name, fn)
-
-                
-
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-        fifn = 'static/imgs/cfi.jpg'
-        cv2.imwrite(fifn, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-        
-        # cv2.imwrite(fifn, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Capture frame-by-frame
+        success, frame = video_capture.read()  # read the camera frame
+        if not success:
             break
+        else:
+            ret, frame = video_capture.read()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
 
-frame_thread = threading.Thread(target=frame_stream_thread, args=(1,))
-frame_thread.start()
+            # Only process every other frame of video to save time
+            if process_this_frame:
+                # Find all the faces and face encodings in the current frame of video
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-# listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-# listener.start()
+                face_names = []
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    name = "Unknown"
 
-while 1:
-    pass
-    # vals = [lr, fb, ud, yv]
-    # me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
-    # sleep(0.05)
+                    # # If a match was found in known_face_encodings, just use the first one.
+                    # if True in matches:
+                    #     first_match_index = matches.index(True)
+                    #     name = known_face_names[first_match_index]
+
+                    # Or instead, use the known face with the smallest distance to the new face
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+
+                    face_names.append(name)
+
+            process_this_frame = not process_this_frame
+
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                if name != 'Unknown':
+                    print(f'Face detected:\t{name}')
+                    if not(name in rec_names):
+                        rec_names[name]=name
+                        print(f'Generating Report for {name}')
+                        fn = f'imgs/report-imgs/{name}-report.jpg'
+                        print(fn)
+                        
+                        generate_report(name, fn)
+
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/telemetry")
+def telemetry():
+    return render_template('telemetry.html')
+
+@app.route("/start_script")
+def start_script():
+    print('Starting Script...')
+    system('py -3 v1.py')
+    return "Start Script"
+
+@app.route("/telemetry_update")
+def telemetry_update():
+    print('updating telemetry info')
+    battery = randint(10,100)
+    speed = randint(0,10)
+    flight_time = randint(0,20)
+    height = randint(0,400)
+    attitude = randint(0,1000)
+
+    with open("current_frame.txt") as file:
+        img = file.read()
+    data = {
+        'battery': battery,
+        'speed': speed,
+        'flight_time': flight_time,
+        'height': height,
+        'attitude': attitude,
+    }
+
+    json_data = jsonify(data)
+
+    return json_data
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    
+    lr, fb, ud, yv = 0, 0, 0, 0
+    # me = tello.Tello()
+    video_capture = cv2.VideoCapture('tv2.mp4')
+
+    # me.connect()
+    # me.streamoff()
+    # sleep(2)
+    # me.streamon()
+
+    speed = 50
+    rec_names = {}
+
+    preSetFacePath = 'imgs/'
+    onlyfiles = [f for f in listdir(preSetFacePath) if isfile(join(preSetFacePath, f))]
+
+    known_face_encodings = []
+    known_face_names = []
+
+    for i in onlyfiles:
+        lable = i.split('.')[0]
+        fn = preSetFacePath + i
+        known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(fn))[0])
+        known_face_names.append(lable)
+
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+
+    app.run(debug=True)
