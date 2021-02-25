9ec319c45a40f6bcaf541738ee691b9dcb0a1601
henokali1 <henokali1@gmail.com>
2021-02-25 11:48:12 +0400

    .

--- py/pg2.py
+++ py/pg2.py
@@ -1,194 +1,50 @@
-from pynput import keyboard
-from time import sleep
-from djitellopy import tello
-import threading
+#
+# --- (c) 01/2020 f41ardu
+#
+# Tello cam using opencv proof of concept
+# issue: huge delay -> Tipp scale down video size for improved performance on Raspberry PI 3+ 
+# May also work with older versions of opencv supporting incomming udp streams. 
+#
+ 
+#import numpy as np
+# import opencv 4.2.0 
 import cv2
-import face_recognition
-import requests
-import numpy as np
-from os import listdir, remove
-from os.path import isfile, join
-
-
-def get_face_db(img_name, img_url):
-    img_data = requests.get(img_url).content
-    with open(img_name, 'wb') as handler:
-        handler.write(img_data)
-
-def download_faces():
-    url = 'https://node-red-arzjk-2021-01-25.eu-gb.mybluemix.net/all-registered-faces-db'
-    resp = requests.get(url=url)
-    data = resp.json()
-
-    # Remove old face images
-    print('Removing old face data')
-    preSetFacePath = 'imgs/'
-    filenames = [f for f in listdir(preSetFacePath) if isfile(join(preSetFacePath, f))]
-    for fn in filenames:
-        remove(preSetFacePath+fn)
-    print('Old face data removed')
-    print('Downloading new face data.....')
-    for i in data:
-        img_url = i['img_url']
-        img_extension = img_url.split('.')[-1]
-        img_name = f"imgs/{i['name']}.{img_extension}"
-        get_face_db(img_name, img_url)
-    print('New face data download completed')
-
-# Download face data from IBM database
-download_faces()
-
-
-lr, fb, ud, yv = 0, 0, 0, 0
-# me = tello.Tello()
-video_capture = cv2.VideoCapture('tv.mp4')
-
-# me.connect()
-# me.streamoff()
-# sleep(2)
-# me.streamon()
-
-speed = 50
-
-
-preSetFacePath = 'imgs/'
-onlyfiles = [f for f in listdir(preSetFacePath) if isfile(join(preSetFacePath, f))]
-
-known_face_encodings = []
-known_face_names = []
-
-for i in onlyfiles:
-    lable = i.split('.')[0]
-    fn = preSetFacePath + i
-    known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(fn))[0])
-    known_face_names.append(lable)
-
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+import time
+from djitellopy import tello
 
 
 
+me = tello.Tello()
 
-def on_press(key):
-    global lr
-    global fb
-    global ud
-    global yv
-    try:
-        if(key.char == 'a') or (key.char == 'A'):
-            yv = -speed
-        if(key.char == 's') or (key.char == 'S'):
-            ud = -speed
-        if(key.char == 'w') or (key.char == 'W'):
-            ud = speed
-        if(key.char == 'd') or (key.char == 'D'):
-            yv = speed
-        if(key.char == 'q') or (key.char == 'Q'):
-            me.land()
-            me.streamoff()
-            pass
-        if(key.char == 'e') or (key.char == 'E'):
-            me.takeoff()
-            pass
-    except AttributeError:
-        if(key == key.left):
-            lr = -speed
-        if(key == key.right):
-            lr = speed
-        if(key == key.up):
-            fb = speed
-        if(key == key.down):
-            fb = -speed
-
-def on_release(key):
-    global lr
-    global fb
-    global ud
-    global yv
-    lr = 0
-    fb = 0
-    ud = 0
-    yv = 0
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-def frame_stream_thread(name):
-    global process_this_frame
-    while True:
-        # frame = me.get_frame_read().frame
-        ret, frame = video_capture.read()
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
+me.connect()
 
 
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+me.streamon()
 
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+telloVideo = cv2.VideoCapture("udp://@0.0.0.0:11111")
+#telloVideo.set(cv2.CAP_PROP_FPS, 3)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+# wait for frame
+ret = False
+# scale down 
+scale = 3
 
-        # Display the resulting image
-        cv2.imshow('Video', frame)
+while(True):
+    # Capture frame-by-framestreamon
+    ret, frame = telloVideo.read()
+    
+    if(ret): 
+    # Our operations on the frame come here
+        height , width , layers =  frame.shape
+        new_h=int(height/scale)
+        new_w=int(width/scale)
+        resize = cv2.resize(frame, (new_w, new_h)) # <- resize for improved performance 
+        # Display the resulting frame
+        cv2.imshow('Tello',resize)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
-
-frame_thread = threading.Thread(target=frame_stream_thread, args=(1,))
-frame_thread.start()
-
-# listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-# listener.start()
-
-while 1:
-    pass
-    # vals = [lr, fb, ud, yv]
-    # me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
-    # sleep(0.05)
+# When everything done, release the capture
+telloVideo.release()
+cv2.destroyAllWindows()
